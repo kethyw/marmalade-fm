3ae9b4b9f63b8257ae48fe29154020b3f54f032e
Kethy Wang <kethyxwang@gmail.com>
2018-10-19 15:57:47 -0700

    initial commit

---
+++ src/components/PlayMix.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// this component wraps around anything when we click 
+// will start playing a mix for us. it provides us functionality
+// rather than any design 
+const PlayMix = ({ playMix, id, currentMix, playing, children }) => (
+  // when our currently playing mix equals the id of the mix 
+  // that this component refers to, we will add a class name
+  // of 'playing' 
+  <div
+    className={`pointer ${id === currentMix && playing && 'playing'}`}
+    onClick={() => playMix(id)}
+  >
+    {children}
+  </div>
+);
+
+export default PlayMix;
---
+++ src/data/mixes.js
@@ -0,0 +1,15 @@
+const mixes = [
+  '/kubozuzula/chillout-with-chopin/',
+  '/adamkvasnica3/17-reasons-to-love-classical/',
+  '/adamkvasnica3/17-reasons-to-love-classical-vol2/',
+  '/Cinambientique/thank-you-jóhann-jóhannsson/',
+  '/sofamixes/piano-works-sweet/',
+  '/vladastojanovic/night-shift-piano/',
+  '/nataljabaskirova/classical-music-compilation/',
+  '/ClassicalMusic/vivaldi-the-four-seasons/',
+  '/penkipenki/55junior-pirmadienis-37-lukas-šidlauskas/',
+  '/michael-e/piano-mix-iv/',
+  '/dannniel/piano-series-vol-4/'
+];
+
+export default mixes;
